3c9dd1736874ed65997d1e53e4f5138fa11ae323
Alex Pineda <h0h04dt@homeoffice.wal-mart.com>
2021-04-24 13:21:35 -0500

    get items

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,3 +1,4 @@
+import pytest
 from fastapi.testclient import TestClient
 
 from app.main import app
@@ -9,3 +10,10 @@ def test_get_root():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"mensaje": "hola mundo"}
+
+
+@pytest.mark.parametrize("input,expected", [(1, 1), (2, 2), (3, 3)])
+def test_get_item_id(input, expected):
+    response = client.get(f"/items/{input}")
+    assert response.status_code == 200
+    assert response.json() == {"item_id": expected}
